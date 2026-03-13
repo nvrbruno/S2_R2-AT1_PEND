01f4e720118e3b64e5b9f20bdefbd4035718c166
BRUNO DAVI NAVARRO <bruno.navarro6@senaisp.edu.br>
2026-03-13 11:00:34 -0300

    Atividade corrigida

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ let btnEnviar = document.querySelector("#btnEnviar")
 let mensagem = document.getElementById("mensagem")
 
 btnEnviar.addEventListener("click", () => {
-btnEnviar.innerText = `Mensagem alterada pelo JS`
+mensagem.innerText = `Mensagem alterada pelo JS`
 })
