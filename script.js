c39cc4bf902be29439f65fbd337b0081b01ce136
BRUNO DAVI NAVARRO <bruno.navarro6@senaisp.edu.br>
2026-03-20 10:12:36 -0300

    Estilizado

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let nomeTarefa = document.getElementById("nomeTarefa")
 let btnCriar = document.querySelector("#btnCriar")
 let mensagem = document.getElementById("mensagem")
-let lista = document.getElementById("lista")
+let listaTarefas = document.getElementById("listaTarefas")
 
 btnCriar.addEventListener("click", () => {
-  let item = document.createElement("li")
-  item.innerText = nomeTarefa.value
-  lista.appendChild(item)
-  mensagem.innerText = `Criado com sucesso`
+    let item = document.createElement("li")
+    item.className = "list-group-item"
+    item.innerText = nomeTarefa.value
+    listaTarefas.appendChild(item)
+    mensagem.className = "text-success fw-bold"
+    mensagem.innerText = "Criado com sucesso"
 })
