82cc85b09b4696d5c16dc393a6567f756c8a3685
BRUNO DAVI NAVARRO <bruno.navarro6@senaisp.edu.br>
2026-03-20 09:55:48 -0300

    Estilizando

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
-let inputTarefa = document.getElementById("inputTarefa")
-let btnAdicionar = document.querySelector("#btnAdicionar")
+let nomeTarefa = document.getElementById("nomeTarefa")
+let btnCriar = document.querySelector("#btnCriar")
 let mensagem = document.getElementById("mensagem")
-let listaTarefas = document.getElementById("listaTarefas")
+let lista = document.getElementById("lista")
 
-btnAdicionar.addEventListener("click", () => {
-
-    if (!inputTarefa.value) {
-        mensagem.innerText = 'Tarefa vazia!'
-        mensagem.className = 'text-danger fw-bold'
-    } else {
-        let item = document.createElement("li")
-        item.className = "list-group-item"
-        item.innerText = inputTarefa.value
-        listaTarefas.appendChild(item)
-        mensagem.className = 'text-success fw-bold'
-        mensagem.innerText = `Criado com sucesso`
-    }
-
+btnCriar.addEventListener("click", () => {
+  let item = document.createElement("li")
+  item.innerText = nomeTarefa.value
+  lista.appendChild(item)
+  mensagem.innerText = `Criado com sucesso`
 })
